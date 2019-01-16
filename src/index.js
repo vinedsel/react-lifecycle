9624e5b8a92c3a06742c9d5b929d6c37bda65804
Raymond Gregory <vinedsel@gmail.com>
2019-01-16 15:15:26 -0600

    Added base project code.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,38 @@
-alert("it's alive");
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = props => {
+  return <BankAccount />;
+};
+
+class BankAccount extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      accountBalance: 2222.22,
+      addAmount: 0
+    };
+  }
+
+  increment() {
+    this.setState({
+      accountBalance: this.state.accountBalance + parseInt(this.state.addAmount)
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>Account Balance: ${this.state.accountBalance}</h3>
+        <input
+          type="number"
+          onChange={event => this.setState({ addAmount: event.target.value })}
+          value={this.state.addAmount}
+        />
+        <button onClick={this.increment.bind(this)}>Increase Amount</button>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
